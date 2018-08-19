07166ad4a9e915d95f5658137ebc09515fe99d28
Kent Ogisu <kentogisu@gmail.com>
2018-08-19 13:59:08 -0400

    [WIP] Added to search action

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,8 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
   //TODO:  Write an asynchronous action to handle a video search!
+  videos: q;
 };
 
 export default handleVideoSearch;
